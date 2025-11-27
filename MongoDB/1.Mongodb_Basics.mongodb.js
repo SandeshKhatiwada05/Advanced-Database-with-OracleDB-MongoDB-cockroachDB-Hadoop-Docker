3baa292576ea0978c322e90ab9886dcc1ceb3ffb
SandeshKhatiwada05 <khatiwadasandesh501@gmail.com>
2025-11-27 08:59:26 +0545

    mongo

---
+++ MongoDB/1.Mongodb_Basics.mongodb.js
@@ -0,0 +1,42 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use("OIC");
+
+// db.lecturers.insertOne({
+//     roll: 102,
+//     name: "Sandesh",
+//     salary:  85000,
+//     age : 44,
+//     hire_date: ISODate('2004-02-03')
+// })
+
+// db.lecturers.insertMany([{
+//     roll: 103,
+//     name: "Saurav",
+//     salary:  85000,
+//     age : 44,
+//     hire_date: ISODate('2004-02-03')
+// },
+// {
+//     roll: 102,
+//     name: "Sandesh 5",
+//     salary:  85000,
+//     age : 44,
+//     hire_date: ISODate('2004-02-03')
+// }])
+
+
+// db.lecturers.deleteOne({roll:101})
+
+// db.lecturers.deleteOne({roll:101})
+
+// db.lecturers.find({_id: 6927bbe97e50852cef27990e});
+
+// db.lecturers.find().sort({ roll: -1 }).limit(1)
+// db.lecturers.find().sort({ roll: -1 }).limit(1).toArray()[0]._id.getTimestamp();
+
+
+
+
