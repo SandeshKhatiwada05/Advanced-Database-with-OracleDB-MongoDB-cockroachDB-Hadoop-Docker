f172f8ced34cfbe0aad71f18c16b1a242bda8665
SandeshKhatiwada05 <khatiwadasandesh501@gmail.com>
2025-12-07 19:06:13 +0545

    list partitioning in oracle

--- Oracle/Sandesh_28936.sql
+++ Oracle/Sandesh_28936.sql
@@ -49,3 +49,82 @@ SELECT * FROM sales1 ORDER BY sales_date;
 SELECT * FROM sales1 PARTITION (p3);
 
 
+
+
+-------------------------------------
+---2. List Partition
+-------------------------------------
+-- Create table partitioned by LIST on region (
+CREATE TABLE sales2
+(
+  customer_id  NUMBER,
+  sales_date   DATE,
+  order_amount NUMBER,
+  region       NVARCHAR2(10)
+)
+PARTITION BY LIST (region)
+(
+  PARTITION p1 VALUES ('East'),
+  PARTITION p2 VALUES ('West'),
+  PARTITION p3 VALUES ('North'),
+  PARTITION p4 VALUES ('South')
+);
+
+
+--insert into list partitioning
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (110, TO_DATE('01-FEB-2015','DD-MON-YYYY'), 200, N'East');
+
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (111, TO_DATE('12-FEB-2015','DD-MON-YYYY'), 350, N'East');
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (112, TO_DATE('25-FEB-2015','DD-MON-YYYY'), 150, N'East');
+
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (120, TO_DATE('05-MAR-2015','DD-MON-YYYY'), 400, N'West');
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (121, TO_DATE('20-MAR-2015','DD-MON-YYYY'), 250, N'West');
+
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (130, TO_DATE('03-APR-2015','DD-MON-YYYY'), 500, N'North');
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (131, TO_DATE('15-APR-2015','DD-MON-YYYY'), 275, N'North');
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (132, TO_DATE('29-APR-2015','DD-MON-YYYY'), 325, N'North');
+
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (140, TO_DATE('07-MAY-2015','DD-MON-YYYY'), 180, N'South');
+INSERT INTO sales2 (customer_id, sales_date, order_amount, region)
+VALUES (141, TO_DATE('21-MAY-2015','DD-MON-YYYY'), 420, N'South');
+COMMIT;
+
+-- Verify: view all rows ordered by region/date
+SELECT * FROM sales2 ORDER BY region, sales_date;
+
+--view partitioned value
+SELECT * FROM sales2 PARTITION (p1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
